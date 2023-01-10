7f14783e7f9642bba690d84ca4066fd693fbaf35
Denis Carriere <carriere.denis@gmail.com>
2023-01-10 15:32:04 -0500

    bump minor version

--- gen.sh
+++ gen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ANTELOPE_SPKG="${ANTELOPE_SPKG:-https://github.com/EOS-Nation/firehose-antelope/releases/download/v0.0.2/antelope-v0.0.2.spkg}"
+VERSION="v0.0.3"
+ANTELOPE_SPKG="${ANTELOPE_SPKG:-https://github.com/EOS-Nation/firehose-antelope/releases/download/$VERSION/antelope-$VERSION.spkg}"
 
 echo "Generating Antelope Protobuf using $ANTELOPE_SPKG"
 substreams protogen "$ANTELOPE_SPKG" --exclude-paths="sf/substreams/v1,google/" --output-path="./src/pb"
