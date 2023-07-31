ff8a3924c5c872e49faa047e8da2eb5981e5a75f
YaroShkvorets <shkvorets@gmail.com>
2023-07-31 15:12:11 -0400

    add type tests

--- src/abigen/ty.rs
+++ src/abigen/ty.rs
@@ -36,3 +36,46 @@ impl Type {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quote::quote;
+    use crate::abigen::abi::{ABIStruct, ABIType};
+    #[test]
+    fn test_type_generate() {
+        let abi_struct = ABIStruct {
+            name: "my_struct".to_string(),
+            fields: vec![
+                ABIType {
+                    name: "bool_field".to_string(),
+                    ty: "bool".to_string(),
+                },
+                ABIType {
+                    name: "name_field".to_string(),
+                    ty: "name".to_string(),
+                },
+                ABIType {
+                    name: "uint64_field".to_string(),
+                    ty: "uint64".to_string(),
+                },
+            ],
+            base: "".to_string(),
+        };
+        let typ = Type::from(abi_struct);
+
+        let generated_tokens = typ.generate();
+
+        let expected_tokens = quote! {
+            #[derive(Debug, Clone, PartialEq, serde::Deserialize)]
+            #[serde(deny_unknown_fields)]
+            pub struct MyStruct {
+                pub bool_field: Bool,
+                pub name_field: Name,
+                #[serde(deserialize_with = "substreams_antelope::decoder::str_or_u64")]
+                pub uint64_field: Uint64
+            }
+        };
+        assert_eq!(generated_tokens.to_string(), expected_tokens.to_string());
+    }
+}
