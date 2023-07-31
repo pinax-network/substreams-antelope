2290aed3d950bd0f24112f45aa39ee0ed8345796
YaroShkvorets <shkvorets@gmail.com>
2023-07-31 09:12:02 -0400

    refactor data structure

--- src/abigen/contract.rs
+++ src/abigen/contract.rs
@@ -1,19 +1,18 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use std::collections::HashMap;
 
 use super::abi::ABI;
 use super::action::Action;
 use super::ty::Type;
 
 pub struct Contract {
-    pub types: HashMap<String, Type>,
+    pub types: Vec<Type>,
     pub actions: Vec<Action>,
 }
 
 impl Contract {
     pub fn generate(&self) -> TokenStream {
-        let types: Vec<_> = self.types.values().filter(|ty| !ty.is_entity).map(Type::generate).collect();
+        let types: Vec<_> = self.types.iter().filter(|ty| !ty.is_entity).map(Type::generate).collect();
         let actions: Vec<_> = self.actions.iter().map(Action::generate).collect();
 
         quote! {
@@ -29,19 +28,19 @@ impl Contract {
 
 impl From<ABI> for Contract {
     fn from(abi: ABI) -> Self {
-        let mut types = abi.structs.into_iter().fold(HashMap::new(), |mut acc, abi_struct| {
-            acc.insert(abi_struct.name.clone(), Type::from(abi_struct));
-            acc
-        });
+        let mut types = abi.structs.into_iter().map(Type::from).collect::<Vec<_>>();
 
         let actions: Vec<Action> = abi
             .actions
             .into_iter()
             .map(|abi_action| {
-                if let Some(ty) = types.get_mut(&abi_action.ty) {
-                    ty.is_entity = true;
+                match types.iter_mut().find(|ty|abi_action.ty == ty.name) {
+                    Some(ty) => {
+                        ty.is_entity = true;
+                        Action::from((abi_action.name, ty.clone()))
+                    }
+                    None => panic!("No type found for action {}", abi_action.name)
                 }
-                Action::from((abi_action.name, types[&abi_action.ty].clone()))
             })
             .collect();
 
